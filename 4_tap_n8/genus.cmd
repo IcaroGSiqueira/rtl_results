1210a2d1edd4429924d2a75f2552f54eb0b96c53
IcaroGSiqueira <icaro.siqueira@sou.ucpel.edu.br>
2020-03-03 17:24:50 -0300

    commit

--- 4_tap_n8/genus.cmd
+++
@@ -1,7 +0,0 @@
-# Cadence Genus(TM) Synthesis Solution, Version 17.10-p007_1, built Aug  3 2017
-
-# Date: Tue Mar 03 13:21:27 2020
-# Host: twins2 (x86_64 w/Linux 2.6.32-754.9.1.el6.x86_64) (4cores*8cpus*1physical cpu*Intel(R) Xeon(R) CPU E3-1271 v3 @ 3.60GHz 8192KB)
-# OS:   CentOS release 6.10 (Final)
-
-source ../../setupe_example.tcl
